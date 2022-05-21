ae6f24875df7143c0bb5efdc2eb3bed79bc24e6a
Christian Hemefor <hemeforc@gmail.com>
2022-05-21 16:07:40 -0600

    Assignment #2

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,28 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+// Task 3 postComment and addComment
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+
+    newComment.date = new Date().toISOString();
+
+    setTimeout(() => { 
+        dispatch(addComment(newComment));
+    }, 
+    
+    2000);
+};
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+        payload: comment
+});
+
